bd74fcecae5adc5a6e24efc2813bf8192b62cd81
DannyLenk <danylenko.14@gmail.com>
2022-02-16 10:00:56 +0200

    added dropdown functionality

--- working-with-APIs/color-scheme-generator/index.js
+++ working-with-APIs/color-scheme-generator/index.js
@@ -6,12 +6,18 @@ document.querySelector('#dropBtn').addEventListener('click', () => {
   myDropdown.classList.toggle('show');
 })
 
-// window.onclick = function(e) {
-//   if (e.target.id != 'dropBtn') {
-//     myDropdown.classList.remove('show');
-//   }
+window.onclick = function(e) {
+  
+  if (e.target.id != 'dropBtn') {
+    myDropdown.classList.remove('show');
+  }
 
-//   if (e.target.classList.contains('dropdown-a')) {
-//     console.log(e.target.textContent);
-//   }
-// } 
+  if (e.target.classList.contains('dropdown-a')) {
+    const aList = document.querySelectorAll('.dropdown-a');
+
+    aList.forEach(a => a.classList.remove('active'));
+    document.querySelector('#schemeBtn').textContent = e.target.textContent;
+    e.target.classList.add('active');
+  }
+
+} 
