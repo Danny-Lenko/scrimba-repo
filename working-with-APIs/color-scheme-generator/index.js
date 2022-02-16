5fdf2213c80d49c483abf48aaa39a1ecbc886d11
DannyLenk <danylenko.14@gmail.com>
2022-02-16 09:43:56 +0200

    added active a

--- working-with-APIs/color-scheme-generator/index.js
+++ working-with-APIs/color-scheme-generator/index.js
@@ -1,2 +1,17 @@
 "use strict"
 
+const myDropdown = document.querySelector('#myDropdown');
+
+document.querySelector('#dropBtn').addEventListener('click', () => {
+  myDropdown.classList.toggle('show');
+})
+
+// window.onclick = function(e) {
+//   if (e.target.id != 'dropBtn') {
+//     myDropdown.classList.remove('show');
+//   }
+
+//   if (e.target.classList.contains('dropdown-a')) {
+//     console.log(e.target.textContent);
+//   }
+// } 
