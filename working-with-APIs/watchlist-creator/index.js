79f84a187e2879b9cb2b8109a2c32ec5a27a223c
DannyLenk <danylenko.14@gmail.com>
2022-02-19 06:38:23 +0200

    added all the styles

--- working-with-APIs/watchlist-creator/index.js
+++ working-with-APIs/watchlist-creator/index.js
@@ -1,13 +1,17 @@
 "use strict"
 
+
+
 document.querySelector('.header__btn').addEventListener('click', async () => {
   let userRequest = document.querySelector('.header__input').value;
-  document.querySelector('.header__input').value = '';
-  userRequest = userRequest.toLowerCase().trim().match(/[a-z0-9]+/g).join("+")
-
-  console.log(userRequest)
-
-  const res = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=d6310ff9&t=${userRequest}`);
-  const data = await res.json();
-  console.log(data);
+  if (userRequest) {
+    document.querySelector('.header__input').value = '';
+    userRequest = userRequest.toLowerCase().trim().match(/[a-z0-9]+/g).join("+")
+  
+    console.log(userRequest)
+  
+    const res = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=d6310ff9&t=${userRequest}`);
+    const data = await res.json();
+    console.log(data);  
+  }
 })
