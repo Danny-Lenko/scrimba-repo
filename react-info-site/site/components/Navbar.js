d070dca3aacce2a911e0edb9d59be2e684312164
DannyLenk <danylenko.14@gmail.com>
2022-03-26 10:58:05 +0200

    navbar layout

--- react-info-site/site/components/Navbar.js
+++ react-info-site/site/components/Navbar.js
@@ -2,6 +2,10 @@ import React from "react"
 
 export default function Navbar() {
     return (
-        <h1>Navbar goes here</h1>
+        <>
+            <img src="../images/react-icon-small.png" width="29px" alt="" />
+            <h2>ReactFacts</h2>  
+            <h3>React Course - Project 1</h3>
+        </>
     )
 }
