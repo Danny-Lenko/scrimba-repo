a1ab88cfe99e2d9780dbccc2d8b49d8d05567cab
DannyLenk <danylenko.14@gmail.com>
2022-03-27 10:03:56 +0300

    full project

--- react-info-site/site/App.js
+++ react-info-site/site/App.js
@@ -1,12 +1,12 @@
 import React from "react"
-import Main from "./components/Main"
 import Navbar from "./components/Navbar"
+import Main from "./components/Main"
 
 export default function App() {
     return (
-        <>
+        <div className="container">
             <Navbar />
             <Main />
-        </>
+        </div>
     )
 }
