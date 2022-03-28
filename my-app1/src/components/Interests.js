050b60385bcf8166883d4866d8d9a660538a98b2
DannyLenk <danylenko.14@gmail.com>
2022-03-28 12:46:12 +0300

    2 more components

---
+++ my-app1/src/components/Interests.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Interest() {
+   return(
+      <section className="interests">
+         <h2 className="interests__h2">Interests</h2>
+         <p className="interests__text">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+      </section>
+   )
+}
