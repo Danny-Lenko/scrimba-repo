97356541e5b472f928d9eb960a082e64a8dab93e
DannyLenk <danylenko.14@gmail.com>
2022-03-28 11:39:28 +0300

    finished info component

--- my-app1/src/components/Info.js
+++ my-app1/src/components/Info.js
@@ -1,13 +1,19 @@
 import React from "react";
+import profileImg from "../images/profile-img.png"
 
 export default function Info() {
    return(
       <header>
-         {/* <img src="" alt="" /> */}
-         <h1 className="header__h1">Laura Smith</h1>
-         <h3 className="header__h3">Frontend Developer</h3>
-         <p className="header__site">laurasmith.website</p>
-         <a href="mailto:laura@smith.dev" className="header__e-mail">Email</a>
+         <img className="header__img" src={profileImg} alt="" />
+         <section className="header__content">
+            <h1 className="header__h1">Laura Smith</h1>
+            <h3 className="header__h3">Frontend Developer</h3>
+            <p className="header__site">laurasmith.website</p>
+            <div className="header__btns-container">
+               <a href="mailto:laura@smith.dev" className="header__e-mail"><i className="fa-solid fa-envelope"></i>Email</a>
+               <a href="" className="header__linked"><i className="fa-brands fa-linkedin"></i>LinkedIn</a>
+            </div>
+         </section>
       </header>
    )
 }
