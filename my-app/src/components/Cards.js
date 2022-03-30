609634a55af68b8ad735c0d9f3de4d1d40932307
DannyLenk <danylenko.14@gmail.com>
2022-03-30 11:38:32 +0300

    completed styling

--- my-app/src/components/Cards.js
+++ my-app/src/components/Cards.js
@@ -7,7 +7,7 @@ export default function Cards() {
          <div className="card__content">
             <p className="card__map-data">
                <span className="card__country"><i className="fa-solid fa-location-dot"></i>Japan</span>
-               <span className="card__coordinates">View on Google Maps</span>
+               <span className="card__coordinates"><a href="#">View on Google Maps</a></span>
             </p>
             <h2 className="card__h2">Mount Fuji</h2>
             <h4 className="card__dates">12 Jan, 2021 - 24 Jan, 2021</h4>
