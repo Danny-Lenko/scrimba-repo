9665b555b1a6459c31f474b3018f89df837f6504
DannyLenk <danylenko.14@gmail.com>
2022-03-30 10:47:57 +0300

    styling process

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Header from "./components/Header"
+import Cards from "./components/Cards"
 
 export default function App() {
    return(
       <div className="container">
          <Header />
+         <Cards />
       </div>
    )
 }
---
+++ my-app/src/components/Cards.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Cards() {
+   return(
+      <div className="card">
+         <img className="card__img" src="https://source.unsplash.com/WLxQvbMyfas" alt="" />
+         <div className="card__content">
+            <p className="card__map-data">
+               <span className="card__country"><i className="fa-solid fa-location-dot"></i>Japan</span>
+               <span className="card__coordinates">View on Google Maps</span>
+            </p>
+            <h2 className="card__h2">Mount Fuji</h2>
+            <h4 className="card__dates">12 Jan, 2021 - 24 Jan, 2021</h4>
+            <p className="card__description">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+         </div>
+      </div>
+   )
+}
