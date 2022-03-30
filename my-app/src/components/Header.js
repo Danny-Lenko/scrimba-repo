eafd99897c8921030a70edef52c5d8d836f59fae
DannyLenk <danylenko.14@gmail.com>
2022-03-30 10:08:29 +0300

    header of new project

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+   return(
+      <header>
+        <i className="fa-solid fa-earth-europe"></i> my travel journal.
+      </header>
+   )
+}
