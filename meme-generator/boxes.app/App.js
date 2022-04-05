e259943c5d96ad2a407b961191b6d0938405f90f
DannyLenk <danylenko.14@gmail.com>
2022-04-05 12:32:10 +0300

    prevSquares.map(square => {

--- meme-generator/boxes.app/App.js
+++ meme-generator/boxes.app/App.js
@@ -5,8 +5,20 @@ import Box from "./Box"
 export default function App() {
     const [squares, setSquares] = React.useState(boxes)
     
+    function toggle(id) {
+        setSquares(prevSquares => {
+            return prevSquares.map((square) => {
+                return square.id === id ? {...square, on: !square.on} : square
+            })
+        })
+    }
+    
     const squareElements = squares.map(square => (
-        <Box key={square.id} on={square.on} />
+        <Box 
+            key={square.id} 
+            on={square.on} 
+            toggle={() => toggle(square.id)}
+        />
     ))
     
     return (
