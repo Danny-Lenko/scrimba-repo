77c9908529d5013cc4dcf77f46c5d5319fed1054
DannyLenk <danylenko.14@gmail.com>
2022-05-23 10:38:27 +0300

    challenges

---
+++ advanced-react/hooks/challenge5.js
@@ -0,0 +1,51 @@
+import React, {useState, useEffect} from "react"
+
+/**
+ * Challenge:
+ *
+ * Make it so clicking the Start button starts the timer instead of it starting on refresh
+ * (Hint: use a new state variable to indicate if the game should be running or not)
+ */
+
+function App() {
+    const [text, setText] = useState("")
+    const [timeRemaining, setTimeRemaining] = useState(2)
+    const [isTimeRunning, setIsTimeRunning] = useState(false)
+
+    function handleChange(e) {
+        const {value} = e.target
+        setText(value)
+    }
+
+    function calculateWordCount(text) {
+        const wordsArr = text.trim().split(" ")
+        return wordsArr.filter(word => word !== "").length
+    }
+
+    useEffect(() => {
+        if(isTimeRunning && timeRemaining > 0) {
+            setTimeout(() => {
+                setTimeRemaining(time => time - 1)
+            }, 1000)
+        } else if(timeRemaining === 0) {
+            setIsTimeRunning(false)
+        }
+    }, [timeRemaining, isTimeRunning])
+
+    return (
+        <div>
+            <h1>How fast do you type?</h1>
+            <textarea
+                onChange={handleChange}
+                value={text}
+            />
+            <h4>Time remaining: {timeRemaining}</h4>
+            <button onClick={() => setIsTimeRunning(true)}>Start</button>
+            <h1>Word count: ???</h1>
+        </div>
+    )
+}
+
+export default App
+
+
